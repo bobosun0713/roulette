b24e6617a7b968eba892c6ebf08e36b9c42e0b4e
bobosun0713 <chiabo.sun@gmail.com>
2022-09-04 23:09:52 +0800

    feat: modify the tailwind RWD config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xxxs: '280px',
+      xxs: '320px',
+      xs: '412px',
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
     extend: {}
   },
   plugins: []
