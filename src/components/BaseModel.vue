f2dd98f3ef951092d4087f94b612019750917f51
bobosun0713 <chiabo.sun@gmail.com>
2022-09-11 23:02:04 +0800

    feat: add model layout

---
+++ src/components/BaseModel.vue
@@ -0,0 +1,53 @@
+<script setup>
+import {} from 'vue';
+const emit = defineEmits(['on-close']);
+const props = defineProps({
+  showModal: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
+
+<template>
+  <transition name="overlay">
+    <div v-show="props.showModal" class="fixed w-[100vw] h-[100vh] z-[50]">
+      <transition name="overlay">
+        <div v-show="props.showModal" class="absolute z-[-1] w-[100%] h-[100%] bg-[#131e2698]" @click="emit('on-close', false)"></div>
+      </transition>
+      <transition name="model">
+        <div
+          v-show="props.showModal"
+          class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] overflow-hidden w-[800px] h-[60vh] rounded-[10px] bg-[#1D2A33]"
+        >
+          <slot name="header">
+            <div class="text-[1.25rem] text-center text-[#fff] tracking-[1px] p-[4px_0] bg-[#4e4376]">Settings</div>
+          </slot>
+          <slot name="body"></slot>
+        </div>
+      </transition>
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+.model-enter-from,
+.model-leave-active {
+  transition: all 1s ease;
+}
+.model-enter-from,
+.model-leave-to {
+  top: 0%;
+  opacity: 0;
+}
+
+.overlay-enter-active,
+.overlay-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.overlay-enter-from,
+.overlay-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/components/RouletteSettingModel.vue
@@ -0,0 +1,76 @@
+<script setup>
+import {} from 'vue';
+import BaseModel from '@/components/BaseModel.vue';
+const props = defineProps({
+  showModel: {
+    type: Boolean,
+    default: false
+  },
+  data: {
+    type: Array,
+    default: () => []
+  }
+});
+</script>
+
+<template>
+  <BaseModel :showModal="props.showModel" v-bind="$attrs">
+    <template #body>
+      <div class="h-[calc(100%-100px)] overflow-y-scroll scrollbar">
+        <ul class="w-[100%] flex flex-col justify-center p-[10px]">
+          <li
+            v-for="(item, idx) in props.data"
+            :key="idx"
+            class="flex items-center justify-between mb-[10px] border-l-[4px] border-[#4e4376] bg-[#0e171d]"
+          >
+            <div class="flex items-center p-[8px_16px] text-[#ebe5e5]">
+              <p class="w-[50px] mr-[10px]">No. {{ idx + 1 }}</p>
+
+              <input v-model.trim="item.price" class="border border-[#5f5244] bg-transparent p-[4px_8px] text-right outline-none" type="text" />
+            </div>
+
+            <div class="flex items-center p-[8px_16px] text-[#fff]">
+              <p class="mr-[10px]">Background</p>
+              <div class="relative overflow-hidden w-[60px] h-[30px] border border-[#5f5244]">
+                <input
+                  v-model.trim="item.background"
+                  class="bg-transparent text-right outline-none absolute top-[-10px] left-[-10px] h-[80px] w-[80px]"
+                  type="color"
+                />
+              </div>
+            </div>
+
+            <div class="flex items-center p-[8px_16px] text-[#fff]">
+              <p class="mr-[10px]">Color</p>
+              <div class="relative overflow-hidden w-[60px] h-[30px] border border-[#5f5244]">
+                <input
+                  v-model.trim="item.color"
+                  class="bg-transparent text-right outline-none absolute top-[-10px] left-[-10px] h-[80px] w-[80px]"
+                  type="color"
+                />
+              </div>
+            </div>
+            <div class="flex items-center p-[8px_16px] text-[#fff]">
+              <button class="hover:text-[#4e4376] cursor-pointer">DELETE</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <button class="block w-[150px] h-[50px] rounded-[50px] m-[0_auto] font-bold text-[1.25rem] text-[#fff] bg-[#4e4376]" @click="clickSave">
+        ADD
+      </button>
+    </template>
+  </BaseModel>
+</template>
+
+<style scoped>
+.scrollbar::-webkit-scrollbar {
+  width: 8px;
+  height: 50px;
+}
+
+.scrollbar::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 10px;
+}
+</style>
