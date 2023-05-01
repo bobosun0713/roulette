751d80eb4139f2cf1acf1bea2ecf4048d54ddd31
bobosun0713 <chiabo.sun@gmail.com>
2023-05-01 16:54:14 +0800

    chore(format): update the settings in prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,9 +1,8 @@
 module.exports = {
-  tabWidth: 2, // 空格縮排
-  semi: true, // 代碼結尾是否加分號
-  trailingComma: 'none', // 末尾不加逗號
-  singleQuote: true, // 使用單引號
-  printWidth: 150, // 超過字數限制換行
-  arrowParens: 'avoid', // 單個參數不加括號 (箭頭函示)
-  bracketSpacing: true // 物件屬性{}號兩邊加上空白
-}
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "none",
+  bracketSpacing: true,
+  arrowParens: "avoid"
+};
