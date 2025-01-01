d7f0f3523048c15dc0c2b218621dc0dd2bef046a
bobosun0713 <chiabo.sun@gmail.com>
2025-01-01 15:33:34 +0800

    chore: modify ext for config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      xxxs: '280px',
-      xxs: '320px',
-      xs: '424px',
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px'
+      xxxs: "280px",
+      xxs: "320px",
+      xs: "424px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px"
     },
     extend: {}
   },
